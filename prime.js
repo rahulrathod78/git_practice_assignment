b8e03268251dd9bc8f148b0d2e398579929dfa97
Rahul Rameshwar Rathod <120775539+rahulrathod78@users.noreply.github.com>
2023-10-06 11:53:44 +0530

    fix

--- prime.js
+++ prime.js
@@ -1,11 +1,11 @@
-let a=9
-let flag=false
-for(let i=1;i<=a;i++){
-    if(a%i!=0){
-    flag=true
+let a=7
+let flag=true
+for(let i=2;i<a;i++){
+    if(a%i==0){
+    flag=false
     }
 }
-console.log(flag)
+
 if(flag==false){
     console.log("not prime");
 }
